51c660c809fa65761c8347979e7707c46a97086d
yuusi <>
2026-06-27 17:20:49 +0900

    スキルツリー、テキストの修正

--- teamB/Assets/_Scripts/SkilTree/NewMonoBehaviourScript.cs
+++ teamB/Assets/_Scripts/SkilTree/NewMonoBehaviourScript.cs
@@ -4,21 +4,24 @@ using System.Collections.Generic;
 public class Prefab_Instantiate : MonoBehaviour
 {
     public GameObject prefab;
+    public Transform player;   // プレイヤー
     public int count = 5;
+    public float spawnRadius = 3f; // プレイヤーからの距離
 
     List<GameObject> objects = new List<GameObject>();
 
     void Start()
     {
         SubPlayer.blood_on = true;
+
         for (int i = 0; i < count; i++)
         {
-            GameObject obj = Instantiate(
-                prefab,
-                new Vector3(i * 2, 0, 0),
-                Quaternion.identity
-            );
+            // 円の中のランダムな位置
+            Vector2 offset = Random.insideUnitCircle * spawnRadius;
 
+            Vector3 spawnPos = player.position + new Vector3(offset.x, offset.y, 0);
+
+            GameObject obj = Instantiate(prefab, spawnPos, Quaternion.identity);
             objects.Add(obj);
         }
     }
