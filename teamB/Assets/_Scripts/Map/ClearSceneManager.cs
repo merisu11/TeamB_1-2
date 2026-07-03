a17a4c6a0f880d7b819d0fc2c2244ff2d7e90dc9
sdt2595sc0016 <sdt2595sc0016@edu.sca.ac.jp>
2026-07-04 04:19:41 +0900

    クリアシーン

--- teamB/Assets/_Scripts/Map/ClearSceneManager.cs
+++ teamB/Assets/_Scripts/Map/ClearSceneManager.cs
@@ -160,7 +160,8 @@ public class ClearSceneManager : MonoBehaviour
     {
         yield return new WaitUntil(() => isArrived && oxygenAnimationDone);
 
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(1.5f);
 
         //両方終わって6秒たったら2つのボタンを出す
         if (titleButton != null) titleButton.SetActive(true);
